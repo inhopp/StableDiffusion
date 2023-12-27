60eb25e2d96ed2403387207ebffcdaf255943d2f
InHo.Jeong <inhonanananna@gmail.com>
2023-12-27 10:22:09 +0900

    add Decoder.py

---
+++ Decoder.py
@@ -0,0 +1,105 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from Attention import SelfAttention
+
+class VAE_AttentionBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.groupnorm = nn.GroupNorm(32, channels)
+        self.attention = SelfAttention(1, channels)
+
+    def forward(self, x):
+        residue = x
+
+        x = self.groupnorm(x)
+
+        n, c, h, w = x.shape
+
+        x = x.view((n, c, h * w))
+
+        # (Batch, Features, Height * Width) -> (Batch, Height * Width, Features)
+        x = x.transpose(-1, -2)
+
+        x = self.attention(x)
+
+        x = x.transpose(-1, -2)
+
+        x = x.view((n, c, h, w))
+
+        x += residue
+
+        return x
+    
+class VAE_ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.groupnorm1 = nn.GroupNorm(32, in_channels)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.groupnorm2 = nn.GroupNorm(32, out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+
+        if (in_channels == out_channels):
+            self.residual_layer = nn.Identity()
+        else:
+            self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0)
+
+    def forward(self, x):
+        residue = x
+
+        x = self.groupnorm1(x)
+        x = F.silu(x)
+        x = self.conv1(x)
+        x = self.groupnorm2(x)
+        x = F.silu(x)
+        x = self.conv2(x)
+
+        return x + self.residual_layer(residue)
+    
+class VAE_Decoder(nn.Sequential):
+    def __init__(self):
+        super().__init__(
+            # (Batch, 4, Height / 8, Width / 8)
+            nn.Conv2d(4, 4, kernel_size=1, padding=0),
+            nn.Conv2d(4, 512, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(512, 512),
+            VAE_AttentionBlock(512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+
+            # (Batch, 512, Height / 4, Width / 4)
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1), 
+            VAE_ResidualBlock(512, 512), 
+            VAE_ResidualBlock(512, 512), 
+            VAE_ResidualBlock(512, 512), 
+            
+            # (Batch, 512, Height / 2, Width / 2)
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            VAE_ResidualBlock(512, 256),
+            VAE_ResidualBlock(256, 256),
+            VAE_ResidualBlock(256, 256), 
+            
+            # (Batch, 512, Height, Width)
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            VAE_ResidualBlock(256, 128),
+            VAE_ResidualBlock(128, 128),
+            VAE_ResidualBlock(128, 128),
+            nn.GroupNorm(32, 128),
+            nn.SiLU(),
+            nn.Conv2d(128, 3, kernel_size=3, padding=1), 
+        )
+
+    def forward(self, x):
+        # Remove the scaling added by the Encoder.
+        x /= 0.18215
+
+        for module in self:
+            x = module(x)
+
+        return x
